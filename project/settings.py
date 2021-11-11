0d39c51692e993649e93a356a73e7f9a0cd3d3f1
iuryliberato <iuryliberato@hotmail.com>
2021-11-11 14:40:58 +0000

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -105,9 +105,6 @@ AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-
 ]
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- project/urls.py
+++ project/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/events/', include('events.urls')),
     path('api/tags/', include('tags.urls')),
-    path('api/accounts/', include('allauth.urls')),
     path('api/reviews/', include('reviews.urls')),
     path('api/auth/', include('jwt_auth.urls')),
     path('api/join/', include('join.urls')),
